732235b83e3a3c78256c0f2797108b6306dce42d
llrs <lluis.revilla@gmail.com>
2019-05-24 18:11:06 +0200

    Add a second run of Reals (improve filters and checks)

--- R/read.R
+++ R/read.R
@@ -7,19 +7,25 @@ library("tidyr")
 library("writexl")
 library("ggpubr")
 library("patchwork")
+library("lubridate")
 
 bd <- read_xls("data/bd_BCN_tnf_biopsies_110119.xls", na = c("n.a.", ""))
 bd_upa <- read_xls("data/M13-740_abbvie_database_210918.xls")
-reals <- read_xls("data/SOX6 CHI3L1 PDGFD PTGDR2_20190516_115511_Results_Export.xls", 
+reals <- read_xls("data/SOX6 CHI3L1 PDGFD PTGDR2_20190516_115511_Results_Export.xls",
     skip = 15, na = c("", "Undetermined"))
+reals2 <- read_xls("data/THY HTR3E RETNLB COL3A1_20190524_165141_Results_Export.xls",
+    skip = 15, na = c("", "Undetermined"))
+reals <- reals2
 upa_codes <- read_xlsx("data/RT UPA DISEASE BLOC 1 2 3 500NG 20UL.xlsx")
+control_codes <- read_xlsx("data/RT CONTROLS 500NG 20 UL.xlsx")
+antiTNF_codes <- read_xlsx("data/RT biopsies BCN bloc A B C D 500 ng en 20 ul.xlsx")
 
 # Clean the original data ####
 
 # Check if some samples are in more than one plate
 multiple_exp <- reals %>%
     group_by(`Sample Name`) %>%
-    summarise(n = n_distinct(`Experiment Name`)) 
+    summarise(n = n_distinct(`Experiment Name`))
 multiple_exp %>%
     group_by(n) %>%
     count()
@@ -39,7 +45,7 @@ wrong_genes <- well_failed %>%
         inner_join(reals) %>%
         select(`Sample Name`, `Target Name`) %>%
         filter(`Target Name` != "BETA ACTINA") %>%
-        distinct()
+        distinct(.keep_all = TRUE)
 
 failed_reference <- well_failed %>%
         filter(failed == 2) %>%
@@ -61,41 +67,44 @@ failed_betas <- well_failed %>%
 
 stopifnot(nrow(failed_betas) == 0)
 
-sd_Ct <- group_by(reals, `Target Name`, `Sample Name`) %>% 
+sd_Ct <- group_by(reals, `Target Name`, `Sample Name`) %>%
     summarise(sd = sd(`Cт`), mean = mean(`Cт`))
 
 
 erroneous <- filter(sd_Ct, `sd` > 0.5 | mean > 25 & !is.na(sd), `Target Name` == "BETA ACTINA")
+if (nrow(erroneous) > 1) {
+  write_xlsx(erroneous, "processed/big_sd.xlsx")
 
-write_xlsx(erroneous, "processed/big_sd.xlsx")
-
-pdf("Figures/histograms.pdf")
-hist(reals$Cт[reals$`Target Name` == "BETA ACTINA"], main = "Beta Actina", xlab = "Ct")
-hist(reals$Cт[reals$`Target Name` == "CHI3L1"], main = "CHI3L1", xlab = "Ct")
-hist(reals$Cт[reals$`Target Name` == "PDGFD"], main = "PDGFD", xlab = "Ct")
-hist(reals$Cт[reals$`Target Name` == "SOX6"], main = "SOX6", xlab = "Ct")
-dev.off()
-
+  pdf("Figures/histograms.pdf")
+  hist(reals$Cт[reals$`Target Name` == "BETA ACTINA"], main = "Beta Actina", xlab = "Ct")
+  hist(reals$Cт[reals$`Target Name` == "CHI3L1"], main = "CHI3L1", xlab = "Ct")
+  hist(reals$Cт[reals$`Target Name` == "PDGFD"], main = "PDGFD", xlab = "Ct")
+  hist(reals$Cт[reals$`Target Name` == "SOX6"], main = "SOX6", xlab = "Ct")
+  dev.off()
+}
 
 # Do you remember some samples that were in more than one sample?
 # Now we remove a plate
-reals <- filter(reals, 
-            !(`Sample Name` == "73w14c" & grepl("290419bcn 102 a 108", `Experiment Name`)), 
-            !(`Sample Name` == "10w14"  & grepl("290419bcn 102 a 108", `Experiment Name`)))
+reals <- filter(reals,
+            !(`Sample Name` %in% c("102w14c", "73w14c")  & grepl("290419bcn 102 a 108", `Experiment Name`)))
 
-reals[reals$`Experiment Name` == "290419 bcn samples plat 3.eds" & 
+reals[reals$`Experiment Name` == "290419 bcn samples plat 3.eds" &
     reals$`Sample Name` == "44w0c", "Sample Name"] <- "41w14"
+
+write.csv(reals, "processed/reals.csv", row.names = FALSE)
 
 # Check the results
 multiple_exp <- reals %>%
     group_by(`Sample Name`) %>%
-    summarise(n = n_distinct(`Experiment Name`)) 
+    summarise(n = n_distinct(`Experiment Name`))
 m <-multiple_exp %>%
     group_by(n) %>%
     count()
 
-stopifnot(m[m$n == 2, "nn"] == 1)
+stopifnot(!any(m$n >= 2))
 
+
+reals$`Sample Name`[grepl("42W", reals$`Sample Name`, ignore.case = TRUE)] <- "48C 52w14c"
 
 preclean <- reals %>%
   filter(`Target Name` != "BETA ACTINA") %>%
@@ -104,10 +113,12 @@ preclean <- reals %>%
   ungroup() %>%
   filter(!is.na(`ΔCт`))
 
+
 antiTNF <- preclean %>%
-  filter(grepl("^[0-9]", `Sample Name`)) %>%
+  filter(grepl("[0-9]w", `Sample Name`, ignore.case = TRUE)) %>%
+  separate(`Sample Name`, c("Id", "Sample Name")) %>%
   mutate(`Sample Name` = tolower(`Sample Name`),
-         Pacient_id = gsub("w.*", "", `Sample Name`),
+         Pacient_id = as.character(as.numeric(gsub("w.*", "", `Sample Name`))),
          Time = str_extract(`Sample Name`, "w[0-9]*"),
          Location = case_when(grepl("i$", `Sample Name`) ~ "ileum",
                               grepl("c$", `Sample Name`) ~ "colon"),
@@ -115,20 +126,51 @@ antiTNF <- preclean %>%
          AU = 2^(-`ΔCт`)*1000) %>%
   select(-`ΔCт`)
 
+# looking at the table
+antiTNF$Location[antiTNF$`Sample Name` == "04w14"] <- "colon"
+
+# To verify
+antiTNF %>%
+  group_by(Id, `Sample Name`) %>%
+  count() %>%
+  ungroup() %>%
+  group_by(Id) %>%
+  count() %>%
+  filter(n != 1)
+
 UPA <- preclean %>%
   filter(grepl("^UP", `Sample Name`)) %>%
-  mutate(`Sample Name` = gsub("^UP ", "UP", `Sample Name`),         
+  mutate(`Sample Name` = gsub("^UP ", "UP", `Sample Name`),
          AU = 2^(-`ΔCт`)*1000) %>%
   separate(`Sample Name`, c("Id", "Pacient_id")) %>%
   select(-`ΔCт`)
+
+# To verify
+UPA %>%
+  group_by(Id, Pacient_id) %>%
+  count() %>%
+  ungroup() %>%
+  group_by(Id) %>%
+  count() %>%
+  filter(n != 1)
+
 controls <- preclean %>%
-  filter(grepl("^C", `Sample Name`)) %>%
+  mutate(`Sample Name` = gsub(" CONT", "CONT", `Sample Name`)) %>%
+  filter(grepl("(^C)|([0-9]+CONT C)", `Sample Name`)) %>%
   mutate(AU = 2^(-`ΔCт`)*1000,
-         Location = case_when(grepl(" C|S", `Sample Name`) ~ "colon",
-                              grepl(" ILI$", `Sample Name`) ~ "ileum"),
-         Patient_id = gsub(" .+$", "", `Sample Name`)) %>%
+         Location = case_when(grepl("[0-9]+ C|S", `Sample Name`) ~ "colon",
+                              grepl(" ILI$", `Sample Name`) ~ "ileum")) %>%
+  separate(`Sample Name`, c("Id", "Patient_id", "Loc"), remove = FALSE) %>%
   select(-`ΔCт`)
 
+# To verify
+controls %>%
+  group_by(Id, `Sample Name`) %>%
+  count() %>%
+  ungroup() %>%
+  group_by(Id) %>%
+  count() %>%
+  filter(n != 1)
 
 # Clean the "databases" ####
 bd2 <- bd %>%
@@ -156,11 +198,12 @@ df <- merge(bd2, antiTNF, all.x = FALSE, all.y = TRUE) %>%
 filter(df, Location != sample_location) %>%
   group_by(Sample_id) %>%
   select(Location, sample_location, `Sample Name`)
+# Should be null!!
 
 ## UPA ####
-duplic <- group_by(bd_upa, BarCode) %>% 
-        summarise(BarCodes = n()) %>% 
-        filter(BarCodes != 1) %>% 
+duplic <- group_by(bd_upa, BarCode) %>%
+        summarise(BarCodes = n()) %>%
+        filter(BarCodes != 1) %>%
         pull(BarCode)
 # Because we have duplicates we remove some data (first filtering for what do we care
 bd_upa <- distinct(select(bd_upa, Week, pSES.CD, BarCode, ContainerName, SubjectID))
@@ -168,7 +211,7 @@ bd_upa <- distinct(select(bd_upa, Week, pSES.CD, BarCode, ContainerName, Subject
 df_upa <- merge(UPA, upa_codes,  by.x = "Id", by.y = "RT TUBE")
 stopifnot(nrow(df_upa) == nrow(UPA))
 # right_join(bd_upa, df_upa  by = c("BarCode" = "nº muestra")
-df_upa <- right_join(bd_upa, df_upa, 
+df_upa <- right_join(bd_upa, df_upa,
             by = c("BarCode" = "nº muestra")) %>%
   mutate(ContainerName = gsub("Biopsy ", "", ContainerName),
          Location = ContainerName,
@@ -207,6 +250,9 @@ sam <- filter(dff, Study == "UPA",
        Location == "colon") %>%
   pull("Sample")
 
+
+stopifnot(nrow(filter(dff, is.na(remission))) == 0)
+
 a <- reals %>%
   filter(grepl("^UP", `Sample Name`)) %>%
   mutate(`Sample Name` = gsub("^UP ", "UP", `Sample Name`)) %>%
@@ -220,67 +266,15 @@ b <- reals %>%
   summarise(undetected = sum(is.na(Cт)), total = n()) %>%
   filter(undetected != 0)
 
-write_xlsx(arrange(b, desc(`Sample Name`), desc(`Target Name`)), 
+write_xlsx(arrange(b, desc(`Sample Name`), desc(`Target Name`)),
     "processed/undetected_samples.xlsx")
-write_xlsx(as.data.frame(a[, 3:7]), 
+write_xlsx(as.data.frame(a[, 3:7]),
     "processed/anormal_high_markers_colon.xlsx")
 write_xlsx(dff, "processed/AU_markers.xlsx")
 
 # Plots ####
-i_antiTNF <- filter(df, sample_location == "ileum") %>%
-  ggplot(aes(remission, AU, col = remission,
-             group = remission)) +
-  geom_boxplot() +
-  geom_jitter(width = 0.25) +
-  facet_wrap(~`Target Name`, scales = "free", nrow = 1) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.x = element_blank()) +
-  labs(caption = "ileum")
 
 today <- format(Sys.time(), "%Y%m%d")
-ggsave(paste0("Figures/", today, "_antiTNF_ileum.png"), plot = i_antiTNF)
-
-c_antiTNF <- filter(df, sample_location == "colon") %>%
-  ggplot(aes(remission, AU, col = remission,
-             group = remission)) +
-  geom_boxplot() +
-  geom_jitter(width = 0.25) +
-  facet_wrap(~`Target Name`, scales = "free", nrow = 1) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.x = element_blank()) +
-  labs(caption = "colon")
-ggsave(paste0("Figures/", today, "_antiTNF_colon.png"), plot = c_antiTNF)
-
-
-df_remitter <- df_upa %>%
-  group_by(BarCode) %>%
-  summarise(remitter = if_else(any(pSES.CD < 5), "Y", "N"))
-
-i_upa <- filter(df_upa, ContainerName == "Ileum") %>%
-  ggplot(aes(remission, AU, col = remission,
-             group = remission)) +
-  geom_boxplot() +
-  geom_jitter(width = 0.25) +
-  facet_wrap(~`Target Name`, scales = "free", nrow = 1) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.x = element_blank()) +
-  labs(caption = "ileum")
-print(i_upa)
-c_upa <- filter(df_upa, ContainerName == "Colon") %>%
-  ggplot(aes(remission, AU, col = remission,
-             group = remission)) +
-  geom_boxplot() +
-  geom_jitter(width = 0.25) +
-  facet_wrap(~`Target Name`, scales = "free", nrow = 1) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.x = element_blank()) +
-  labs(caption = "colon")
-print(c_upa)
-
 my_comparisons <- list(c("C C", "w0 TNF"),
                        c("C C", "w0 UPA"),
                        c("w0 UPA", "w0 TNF"),
@@ -304,51 +298,12 @@ preplot %>%
   expand_limits(y = 0) +
   geom_errorbar(aes(ymin = ymin, ymax = ymax), width = 0.2) +
   geom_line() +
-  geom_hline(data = filter(preplot, Study == "C"), 
+  geom_hline(data = filter(preplot, Study == "C"),
                 aes(yintercept = ymin), linetype = "dotted") +
-  geom_hline(data = filter(preplot, Study == "C"), 
+  geom_hline(data = filter(preplot, Study == "C"),
                 aes(yintercept = ymax), linetype = "dotted") +
   facet_wrap(Location ~ Target, scales = "free", ncol = 4) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         axis.title.x = element_blank()) +
   ylab("AU (mean\u00B1SEM)")
-
-ile <- preplot %>%
-  filter(Location == "ileum") %>%
-  ggplot(aes(condition, AU, col = Study)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_point(position = position_jitterdodge()) + # https://stackoverflow.com/a/24019668/2886003
-  facet_wrap( ~ Target, scales = "free", nrow = 1) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank()) +
-  labs(title = "ileum") +
-  stat_compare_means(comparisons = my_comparisons, method = "wilcox", tip.length = 0.05) +
-  scale_color_manual(values = cols)
-
-col + ile
-
-# Table for reporting ####
-bd3 <- bd %>%
-  mutate(Sample_id = tolower(Sample_id)) %>%
-  filter(str_detect(Sample_id, "-w")) %>%
-  mutate(Sample_id = gsub(" reseq|rep", "", Sample_id),
-         Pacient_id = as.character(as.numeric(gsub("-w.*", "", Sample_id))),
-         Time = str_extract(Sample_id, "w[0-9]*"),
-         Sample_id = paste0(Pacient_id, "-", Time)) %>%
-  filter(Time %in% c("w0", "w14")) %>%
-  as.data.frame() %>%
-  select(Location, `Diagnostic age`, `Gender`, `DOB`, `CRP (mg/dL)`, `CDAI CD`,
-         `Perianal disease`, `biopsied_segment`, `Sample_id`, `Treatment`,
-         `Diagnostic age`, sample_date, CDEIS_partial)
-
-df2 <- base::merge(bd3, reals2, all.x = FALSE, all.y = TRUE, by = "Sample_id")
-
-# TODO Finish the tables
-df2 %>%
-  distinct(Pacient_id, .keep_all = TRUE) %>%
-  NULL
-
-
