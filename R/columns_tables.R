435bf6c8e3395dedd7f3d5bc652aeb9042c650e9
llrs <lluis.revilla@gmail.com>
2019-06-21 10:48:44 +0200

    Add column of week 0 remission at week 14

--- R/columns_tables.R
+++ R/columns_tables.R
@@ -6,14 +6,27 @@ bd <- read_xls("data/bd_BCN_tnf_biopsies_110119.xls", na = c("n.a.", ""))
 upa <- read_xls("data/M13-740_abbvie_database_300519.xls", na = c("n.a.", ""))
 
 dff <- read_xlsx("processed/AU_markers.xlsx")
-upa2 <- mutate(upa, PCR_UPA_paper = if_else(BarCode %in% dff$Sample, "yes", "no"))
+upa2 <- mutate(upa, PCR_UPA_paper = if_else(BarCode %in% dff$Sample, "yes", "no"),
+               SubjectID = as.character(SubjectID),
+               cd = tolower(gsub("Container", "", ContainerName))) %>%
+  left_join(dff[, c("Sample", "Patient", "General_location", "remission")],
+            by = c("BarCode" = "Sample", "SubjectID" = "Patient", "cd" = "General_location")) %>%
+  select(-cd) %>%
+  distinct()
 bd2 <- mutate(bd, PCR_UPA_paper = if_else(
   gsub("^0| reseq", "", tolower(Sample_id)) %in% dff$Sample,
-                                          "yes", "no"))
+                                          "yes", "no"),
+  s = gsub("^0| reseq", "", tolower(Sample_id))) %>%
+  left_join(dff[, c("Sample", "Patient", "remission", "General_location")],
+            by = c("Pacient_id" = "Patient",
+                   "sample_location" = "General_location",
+                   "s" = "Sample")) %>%
+  select(-s) %>%
+  distinct()
 
-write.xlsx(upa2, "processed/M13-740_abbvie_database_170619.xlsx", keepNA = FALSE,
+write.xlsx(upa2, "processed/M13-740_abbvie_database_210619.xlsx", keepNA = FALSE,
            firstRow = TRUE, colNames = TRUE)
-write.xlsx(bd2, "processed/bd_BCN_tnf_biopsies_170619.xlsx", keepNA = FALSE,
+write.xlsx(bd2, "processed/bd_BCN_tnf_biopsies_210619.xlsx", keepNA = FALSE,
            firstRow = TRUE, colNames = TRUE)
 
 bd3 <- select(bd2, -c(grep("\\.", colnames(bd2), value = TRUE), "fastq_file")) %>%
